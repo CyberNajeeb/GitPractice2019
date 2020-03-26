c23e32636e0d50fb9511f810f6b32c3530f8f403
mohammedsohrabi <najeebsohrabi@gmail.com>
2020-03-25 21:52:07 -0700

    After os update

--- src/StringPractice.java
+++ src/StringPractice.java
@@ -1,7 +1,6 @@
 public class StringPractice {
     public static void main(String[] args) {
 
-        System.out.println("args = " + args);
-        System.out.println(123);
+        System.out.println("Hello, world!");
     }
 }
